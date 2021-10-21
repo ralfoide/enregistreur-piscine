29fca5dceb05a2b5cb1a9808c7fe9f132eef259e
ralf <ralfoide@gmail.com>
2021-10-21 14:14:20 -0700

    Hublot: Skeleton React+Bootstrap page.

---
+++ react/hublot/src/RPApp.js
@@ -0,0 +1,20 @@
+import "./RPApp.css"
+import React from "react"
+import { HashRouter as Router, Switch, Route } from "react-router-dom"
+import Container from "react-bootstrap/Container"
+import RPContent from "./RPContent"
+import RPHeader from "./RPHeader"
+
+const RPApp = () => {
+    return (
+        <div className="RPApp">
+            <RPHeader/>
+            <Router>
+                <Route path="/" render={props => {
+                    return <RPContent /> } } />
+            </Router>
+        </div>
+  )
+}
+
+export default RPApp
---
+++ react/hublot/src/RPContent.js
@@ -0,0 +1,20 @@
+import React from "react"
+import Container from "react-bootstrap/Container"
+import Jumbotron from "react-bootstrap/Jumbotron"
+
+const RPContent = () => {
+    return (
+        <div>
+            <Jumbotron fluid>
+                <Container>
+                    <h1> R-Piscine </h1>
+                </Container>
+            </Jumbotron>
+            <Container>
+                Placeholder main content
+            </Container>
+        </div>
+  )
+}
+
+export default RPContent
