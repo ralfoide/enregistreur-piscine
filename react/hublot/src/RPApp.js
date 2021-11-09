2d6b1832e096e129fac25e5ea67f768902203713
ralf <ralfoide@gmail.com>
2021-11-08 18:21:26 -0800

    Hublot: Force display to use FR/Paris timezone, no matter what the current browser says.

--- react/hublot/src/RPApp.js
+++ react/hublot/src/RPApp.js
@@ -5,6 +5,13 @@ import RPEvents from "./RPEvents"
 import RPConstants from "./RPConstants"
 import RPMachine from "./RPMachine"
 import RPFooter from "./RPFooter"
+import Moment from "react-moment"
+import "moment-timezone"    // required for default timezone support below
+
+// Indicates we want to process everything with a fr locale format.
+// Also our default timezone is FR, not the current browser one (we use epoch values with a tz)
+Moment.globalLocale = "fr"
+Moment.globalTimezone = "Europe/Paris"
 
 RPConstants.init()
 
