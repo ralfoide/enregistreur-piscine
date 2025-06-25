4c9cde54fc9c2c7720aaad73576c642a195c1040
ralf <ralfoide@gmail.com>
2025-06-25 10:49:49 +0200

    Hublot2: Adjust deployment.

--- react/hublot2/vite.config.ts
+++ react/hublot2/vite.config.ts
@@ -6,12 +6,14 @@ export default defineConfig({
   plugins: [react()],
   // "server" is used when running via "npm run dev".
   server: {
+    host: "0.0.0.0",
     port: 3000,
     strictPort: true,
   },
   // "preview" is used when running via "npm run preview"
   preview: {
-    port: 8080,
+    host: "0.0.0.0",
+    port: 8070,
     strictPort: true,
   },
 })
